b46fcad1c830805e89d1a9ac552dd4c005733adc
Sophia <qyoung@ucsd.edu>
2019-02-26 18:39:10 -0800

    Initial Commit

---
+++ IOS_Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Twitter
+//
+//  Created by Sophia Sun on 2/26/19.
+//  Copyright © 2019 Sophia Qi Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
