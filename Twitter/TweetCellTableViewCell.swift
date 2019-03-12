e2912c77252f509066967149ebe42dc637ed60ab
Sophia <qyoung@ucsd.edu>
2019-03-11 22:55:57 -0700

    retweet

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -19,7 +19,7 @@ class TweetCellTableViewCell: UITableViewCell {
     
     var favorited: Bool = false
     var tweetId:Int = -1
-    
+
     @IBAction func favoriteTweet(_ sender: Any) {
         let toBeFavorited = !favorited
         if(toBeFavorited){
@@ -34,7 +34,11 @@ class TweetCellTableViewCell: UITableViewCell {
         
     }
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId:tweetId, success:{
+            self.setRetweeted(true)
+        },failure:{(error)in
+            print("Error is retweeting: \(error)")
+        })
     }
    
     
@@ -49,6 +53,15 @@ class TweetCellTableViewCell: UITableViewCell {
         }
     }
     
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }else{
+            retweetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
